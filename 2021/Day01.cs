0711043ab9ec1f564d9e8517e2381a90f8bfb394
Jakob Arosenius <jakob.arosenius@packsize.com>
2021-12-01 11:17:23 +0100

    Initial day 1 solution

--- 2021/Day01.cs
+++ 2021/Day01.cs
@@ -16,12 +16,24 @@ namespace Advent.y2021
         public override async Task<long> Part1(IEnumerable<string> input)
         {
             await Task.CompletedTask;
-            return 0;
+            return GetIncreases(input.Select(n => int.Parse(n)).ToList());
         }
         public override async Task<long> Part2(IEnumerable<string> input)
         {
+            var threeMeasure = new List<int>();
+            var nums = input.Select(n => int.Parse(n)).ToList();
+            var _ = nums.Select((n, idx) => {
+                if(nums.Skip(idx).Count() >= 3)
+                    threeMeasure.Add(nums.Skip(idx).Take(3).Sum());
+                return 0;
+            }).ToList();
             await Task.CompletedTask;
-            return 0;
+            return GetIncreases(threeMeasure);
+        }
+
+        private long GetIncreases(List<int> nums)
+        {
+            return nums.Zip(nums.Skip(1), (previous, current) => current > previous ? 1 : 0).Sum();
         }
     }
 }
