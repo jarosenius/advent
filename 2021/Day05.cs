6004dc8530341ec5ef0e8ceb474a1cbea370838a
jarosenius <jakob@arosenius.com>
2021-12-05 08:45:13 +0100

    Day 5

--- 2021/Day05.cs
+++ 2021/Day05.cs
@@ -15,11 +15,124 @@ namespace Advent.y2021
 
         public override long Part1(IEnumerable<string> input)
         {
-             return 0;
+            var vents = GetVents(input, true);
+            return CountOverlappingAtleastNTimes(vents, 2);
         }
+
         public override long Part2(IEnumerable<string> input)
         {
-             return 0;
+            var vents = GetVents(input, false);
+            return CountOverlappingAtleastNTimes(vents, 2);
+        }
+
+        private long CountOverlappingAtleastNTimes(List<Vent> vents, int n)
+        {
+            var coordinates = vents.SelectMany(v => v.AllCoordinates).ToList();
+            
+            var width = coordinates.Max(v => v.X)+1;
+            var length = coordinates.Max(v => v.Y)+1;
+            var grid = new int[length][];
+            for (int i = 0; i < grid.Length; i++)
+            {
+                grid[i] = new int[width];
+            }
+            coordinates.ForEach(c =>  grid[c.Y][c.X]+=1);
+            return grid.Sum(r => r.Count(c => c >= n));
+        }
+
+        private List<Vent> GetVents(IEnumerable<string> input, bool skipDiagonal)
+        {
+            List<Vent> res = new();
+
+            foreach (var line in input)
+            {
+                var parts = line.Split(" -> ");
+                var start = parts[0].Split(",").Select(int.Parse).ToArray();
+                var end = parts[1].Split(",").Select(int.Parse).ToArray();
+
+                res.Add(new Vent(start[0], start[1], end[0], end[1]));
+            }
+            return skipDiagonal ? res.Where(v => v.IsDiagonal == false).ToList() : res;
+        }
+
+        private class Vent
+        {
+            public Coordinate Start { get;set; }
+            public Coordinate End { get;set; }
+            private List<Coordinate> allCoordinates;
+            public List<Coordinate> AllCoordinates => allCoordinates ?? (allCoordinates = GetAllCoordinates());
+            public bool IsHorizontal => Start.Y == End.Y;
+            public bool IsVertical => Start.X == End.X;
+            public bool IsDiagonal => !IsHorizontal && !IsVertical;
+            public Vent(int startX, int startY, int endX, int endY)
+            {
+                Start = new Coordinate(startX, startY);
+                End = new Coordinate(endX, endY);
+
+                if(End.X < Start.X)
+                {
+                    var tmp = Start;
+                    Start = End;
+                    End = tmp;
+                }
+            }
+
+            private List<Coordinate> GetAllCoordinates()
+            {
+                if(IsHorizontal)
+                {
+                    var min = Math.Min(Start.X, End.X);
+                    var max = Math.Max(Start.X, End.X);
+                    var count = max - min;
+                    return Enumerable.Range(min, count + 1).Select(x => new Coordinate(x, Start.Y)).ToList();
+                }
+                else if(IsVertical)
+                {
+                    var min = Math.Min(Start.Y, End.Y);
+                    var max = Math.Max(Start.Y, End.Y);
+                    var count = max - min;
+                    return Enumerable.Range(min, count + 1).Select(y => new Coordinate(Start.X, y)).ToList();
+                }
+
+                // IsDiagonal
+
+                var minX = Math.Min(Start.X, End.X);
+                var maxX = Math.Max(Start.X, End.X);
+                var minY = Math.Min(Start.Y, End.Y);
+                var maxY = Math.Max(Start.Y, End.Y);
+                var countX = Math.Abs(maxX - minX);
+                var countY = Math.Abs(maxY - minY);
+
+                if(countX != countY)
+                    throw new ArgumentException("Cannot handle this type of diagonal");
+
+                if(End.Y < Start.Y)
+                {
+                    if(Start.X < End.X)
+                    {
+                        var x = maxX;
+                        return Enumerable.Range(minY, countY + 1).Select(y => new Coordinate(x--, y)).ToList();
+                    }
+                    else
+                    {
+                        var x = minX;
+                        return Enumerable.Range(minY, countY + 1).Select(y => new Coordinate(x++, y)).ToList();
+                    }
+                }
+
+                var y = minY;
+                return Enumerable.Range(minX, countX + 1).Select(x => new Coordinate(x, y++)).ToList();    
+            }
+        }
+        private class Coordinate
+        {
+            public int X { get;set; }
+            public int Y { get;set; }
+            public Coordinate(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
         }
     }
 }
