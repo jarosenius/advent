c7dc4c7268b47456094a7ffad56336ac013f9b3a
jarosenius <jakob@arosenius.com>
2021-12-06 07:01:22 +0100

    Day 6

--- 2021/Day06.cs
+++ 2021/Day06.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Concurrent;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -15,11 +16,76 @@ namespace Advent.y2021
 
         public override long Part1(List<string> input)
         {
-             return 0;
+            var lanterns = input.First().SplitByAndParseToInt(",").Select(n => new Lanternfish(n, false)).ToList();
+
+            for (int i = 0; i < 80; i++)
+            {
+                var newLanterns = lanterns.Count(l => l.Tick() == true);
+                for (int j = 0; j < newLanterns; j++)
+                {
+                    lanterns.Add(new Lanternfish());
+                }
+            }
+
+            return lanterns.Count();
         }
         public override long Part2(List<string> input)
         {
-             return 0;
+            var lanterns = input.First().SplitByAndParseToInt(",").GroupBy(n => n).ToDictionary(g => g.Key, g => (long)g.Count());    
+            for (int i = 0; i < 256; i++)
+            {
+                lanterns = Tick(lanterns);
+            }
+            return lanterns.Values.Sum();
+        }
+
+        internal static Dictionary<int, long> Tick(Dictionary<int, long> lanterns)
+        {
+            return new Dictionary<int, long>()
+            {
+                [8] = lanterns.GetValueOrDefault(0),
+                [7] = lanterns.GetValueOrDefault(8),
+                [6] = lanterns.GetValueOrDefault(7) + lanterns.GetValueOrDefault(0),
+                [5] = lanterns.GetValueOrDefault(6),
+                [4] = lanterns.GetValueOrDefault(5),
+                [3] = lanterns.GetValueOrDefault(4),
+                [2] = lanterns.GetValueOrDefault(3),
+                [1] = lanterns.GetValueOrDefault(2),
+                [0] = lanterns.GetValueOrDefault(1),
+            };
+        }
+
+        private class Lanternfish
+        {
+            public bool IsNew {get;set;} = false;
+            public int InternalTimer {get;set;}
+            public Lanternfish(int initialInternalTimer, bool isNew)
+            {
+                InternalTimer = initialInternalTimer;
+                IsNew = isNew;
+                if(isNew)
+                    InternalTimer+=2;
+            }
+            public Lanternfish() : this(6, true)
+            {
+
+            }
+
+            public bool Tick()
+            {
+                if(InternalTimer == 0)
+                {
+                    InternalTimer = 6;
+                    return true;
+                }
+                else if(InternalTimer <= 6)
+                {
+                    IsNew = false;
+                }
+
+                InternalTimer--;
+                return false;
+            }
         }
     }
 }
