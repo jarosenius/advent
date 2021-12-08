9a140852c1494e6d8ad91260d730d7e05e273d7b
jarosenius <jakob@arosenius.com>
2021-12-08 07:48:39 +0100

    Day 8 part 1

--- 2021/Day08.cs
+++ 2021/Day08.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
-
 namespace Advent.y2021
 {
     public class Day08 : Day
@@ -15,7 +12,15 @@ namespace Advent.y2021
 
         public override long Part1(List<string> input)
         {
-             return 0;
+            var parts = input.Select(x => x.Split("|"));
+            var rows = parts.Select(p => (pattern: p[0].Split(' ', StringSplitOptions.RemoveEmptyEntries), output: p[1].Split(' ', StringSplitOptions.RemoveEmptyEntries))).ToList();
+
+             return rows.Sum(r => r.output
+                .Count(c => 
+                    c.Length == 2 || 
+                    c.Length == 3 ||
+                    c.Length == 4 ||
+                    c.Length == 7));
         }
         public override long Part2(List<string> input)
         {
