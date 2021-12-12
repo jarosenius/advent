549611a4ec0e4a0740c89a5877febb06834f4002
jarosenius <jakob@arosenius.com>
2021-12-12 12:39:30 +0100

    Day 12 part 2

--- 2021/Day12.cs
+++ 2021/Day12.cs
@@ -8,14 +8,13 @@ namespace Advent.y2021
 {
     public class Day12 : Day
     {
-        ConcurrentDictionary<string, Cave> Caves;
         public Day12() : base(12, 2021)
         {
         }
 
         public override long Part1(List<string> input)
         {
-            Caves = new();
+            ConcurrentDictionary<string, Cave> Caves = new();
             input.ForEach(l => 
             {
                 var parts = l.Split('-');
@@ -28,16 +27,19 @@ namespace Advent.y2021
                 Caves.TryRemove(d.Name, out _);
             });
             Caves.TryGetValue("start", out var start);
-            var sum = 0L;
-            start.ConnectedCaves.ForEach(c =>
-            {
-                sum += c.GetPathsToEnd(new List<string>(){"start"});
-            });
-            return sum;
+            return start.ConnectedCaves.Sum(c => c.GetPathsToEnd(new List<string>(){"start"}, false));
         }
         public override long Part2(List<string> input)
         {
-             return 0;
+            ConcurrentDictionary<string, Cave> Caves = new();
+            input.ForEach(l => 
+            {
+                var parts = l.Split('-');
+                var caves = parts.Select(c => Caves.GetOrAdd(c, nc => new Cave(nc)));
+                caves.ElementAt(0).AddConnectedCave(caves.ElementAt(1));
+            });
+            Caves.TryGetValue("start", out var start);
+            return start.ConnectedCaves.Sum(c => c.GetPathsToEnd(new List<string>(){"start"}, true));
         }
 
         private class Cave
@@ -72,26 +74,40 @@ namespace Advent.y2021
                 cave.RemoveConnectedCave(this);
             }
 
-            public long GetPathsToEnd(List<string> visits)
+            public long GetPathsToEnd(List<string> visits, bool visitTwice)
             {
                 if(IsEnd)
                     return 1;
-                    
-                if(visits.Contains(Name) && !IsLarge)
-                    return 0;
-                var sum = 0L;
-                var filtered = ConnectedCaves.Where(c => !c.IsStart && (c.IsLarge || !visits.Contains(c.Name)));
-                visits.Add(Name);
 
-                filtered.ForEach(c =>
-                {
-                    var s = c.GetPathsToEnd(visits.ToList());
-                    
-                    sum += s;
-                });
-                
-                return sum;
+                if(CanVisit(visits, visitTwice) == false)
+                    return 0;
+
+                visits.Add(Name);
+                return ConnectedCaves.Where(c => 
+                            !c.IsStart 
+                            && (c.IsLarge || (c.CanVisit(visits, visitTwice))))
+                        .Sum(c => c.GetPathsToEnd(visits.ToList(), visitTwice));
             }
+            private bool CanVisit(List<string> visits, bool visitTwice)
+            {
+                if(IsLarge)
+                    return true;
+                
+                if(visitTwice == false && visits.Contains(Name))
+                    return false;
+                else if(visitTwice && visits.Count(v => v == Name) == 2)
+                    return false;
+                else if(visitTwice && visits.Contains(Name) && !CanVisitSmallCave(visits))
+                    return false;
+                return true;
+            }
+
+            private bool CanVisitSmallCave(List<string> visits)
+            {
+                var smallCaveVisits = visits.Where(v => char.IsLower(v[0]));
+                return smallCaveVisits.Count() == smallCaveVisits.Distinct().Count();
+            }
+
         }
     }
 }
