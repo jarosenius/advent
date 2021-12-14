93a3398233ce94886a43bcd73b23f60016a65e20
Jakob Arosenius <jakob.arosenius@packsize.com>
2021-12-14 10:12:59 +0100

    Day 14 part 1

--- 2021/Day14.cs
+++ 2021/Day14.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Advent.y2021
 {
@@ -15,11 +13,25 @@ namespace Advent.y2021
 
         public override long Part1(List<string> input)
         {
-             return 0;
+            var template = input.First();
+            var pairInserts = input.Skip(2).Select(p => p.Split(" -> ")).ToDictionary(p => p[0], p => p[1]);
+            Enumerable.Range(0, 10).ForEach(i => template = ApplyInsertion(template, pairInserts));
+            var groups = template.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+            
+            return groups.Values.Max() - groups.Values.Min();
         }
         public override long Part2(List<string> input)
         {
-             return 0;
+            return 0;
+        }
+
+        private string ApplyInsertion(string template, Dictionary<string, string> inserts)
+        {
+            List<(string Text, int At)> toInsert2 = new();
+            var pairs = template.Zip(template.Skip(1)).Select(p => $"{p.First}{p.Second}");
+            var num = 0;
+            var toInsert = pairs.Select((p, i) => (Text: inserts[p], At: i+1+(num++)));
+            return toInsert.Aggregate(template, (ac, i) => ac.Insert(i.At, i.Text));
         }
     }
 }
