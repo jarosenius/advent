15b23df943cef9d0bd851c7e306bb5c7d6fc0faa
jarosenius <jakob@arosenius.com>
2022-12-03 14:18:39 +0100

    Dag 2

--- 2022/Day02.cs
+++ 2022/Day02.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace Advent.y2022
 {
@@ -7,16 +9,51 @@ namespace Advent.y2022
     {
         public Day02() : base(2, 2022)
         {
-            
+
         }
 
         public override long Part1(List<string> input)
         {
-            return 0;
+            var scores = new Dictionary<string, int>()
+            {
+                {"XA", 1+3},
+                {"XB", 1+0},
+                {"XC", 1+6},
+                {"YA", 2+6},
+                {"YB", 2+3},
+                {"YC", 2+0},
+                {"ZA", 3+0},
+                {"ZB", 3+6},
+                {"ZC", 3+3},
+            };
+
+            // var sum = 0;
+            // foreach(var row in input) 
+            // {
+            //     var move = $"{row[2]}{row[0]}";
+            //     sum += scores[move];
+            // }
+
+            // var summa = (from row in input select scores[$"{row[2]}{row[0]}"]).Sum();
+
+            return input.Sum(row => scores[$"{row[2..]}{row[..1]}"]);
         }
         public override long Part2(List<string> input)
         {
-            return 0;
+            var scores = new Dictionary<string, int>()
+            {
+                {"XA", 0+3},
+                {"XB", 0+1},
+                {"XC", 0+2},
+                {"YA", 3+1},
+                {"YB", 3+2},
+                {"YC", 3+3},
+                {"ZA", 6+2},
+                {"ZB", 6+3},
+                {"ZC", 6+1},
+            };
+
+            return (from row in input select scores[$"{row[2]}{row[0]}"]).Sum();
         }
     }
 }
