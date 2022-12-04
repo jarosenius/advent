29ada1e4945a03d0df9f823396ff9bfa04385e44
jarosenius <jakob@arosenius.com>
2022-12-04 08:55:07 +0100

    Day 3

--- 2022/Day03.cs
+++ 2022/Day03.cs
@@ -12,11 +12,24 @@ namespace Advent.y2022
 
         public override long Part1(List<string> input)
         {
-            return 0;
+            return input
+                .Select(row => row.Chunk(row.Length/2))
+                .Select(CommonItemsPriority).Sum();
         }
         public override long Part2(List<string> input)
         {
-            return 0;
+            return input
+                .Chunk(3)
+                .Select(CommonItemsPriority)
+                .Sum();
+        }
+
+        private int CommonItemsPriority(IEnumerable<IEnumerable<char>> items)
+        {
+            return items.First()
+            .Where(item => items.All(comp => comp.Contains(item)))
+            .Select(item => item > 'a' ? item - 'a' + 1 : item - 'A' + 27)
+            .First();
         }
     }
 }
