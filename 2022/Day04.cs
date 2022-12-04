3bdab2830c8a101ca30c8dd3c9436ad6a84e565f
jarosenius <jakob@arosenius.com>
2022-12-04 09:33:52 +0100

    Dat 4 part 1

--- 2022/Day04.cs
+++ 2022/Day04.cs
@@ -12,11 +12,26 @@ namespace Advent.y2022
 
         public override long Part1(List<string> input)
         {
-            return 0;
+            var sections = input
+                .Select(row => row.Split(',')
+                    .Select(s => s.Split('-')
+                    .Select(int.Parse))
+                    .Select(s => new Section(s.First(), s.Last())));
+
+            return sections.Count(sec => 
+            {
+                var f = sec.First();
+                var t = sec.Last();
+
+                return f.From <= t.From && t.To <= f.To || 
+                        t.From <= f.From && f.To <= t.To;
+            });
         }
         public override long Part2(List<string> input)
         {
             return 0;
         }
+
+        record Section(int From, int To);
     }
 }
