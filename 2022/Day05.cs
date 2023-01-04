9097b31348909c11012649dfbe21a494cfa2ccff
jarosenius <jakob@arosenius.com>
2023-01-04 14:46:36 +0100

    Part 2

--- 2022/Day05.cs
+++ 2022/Day05.cs
@@ -13,19 +13,33 @@ public class Day05 : Day
 
     public override object Part1(List<string> input)
     {
+        var (stacks, instructions) = GetStacksAndInstructions(input);
+        instructions.ForEach(instr => Enumerable.Range(0, instr.CratesToMove).ForEach(_ => instr.To.Push(instr.From.Pop())));
+        return string.Join("", stacks.Select(s => s.Pop()));
+    }
+
+    public override object Part2(List<string> input)
+    {
+        var (stacks, instructions) = GetStacksAndInstructions(input);
+        instructions.ForEach(instr =>
+        {
+            var keepOrder = Enumerable.Range(0, instr.CratesToMove).Select(_ => instr.From.Pop()).Reverse();
+            keepOrder.ForEach(i => instr.To.Push(i));
+        });
+        return string.Join("", stacks.Select(s => s.Pop()));
+    }
+
+
+    private (Stack<char>[] Stacks, IEnumerable<MoveInstruction> Instructions) GetStacksAndInstructions(List<string> input)
+    {
         var crates = input.TakeWhile(i => i != string.Empty);
         var stacks = crates.Last().Chunk(4).Select(c => new Stack<char>()).ToArray();
         var cratesWithLetter = crates.Reverse().Skip(1).SelectMany(c => stacks.Zip(c.Chunk(4)));
         cratesWithLetter.Where(c => c.Second[1] != ' ').ForEach(c => c.First.Push(c.Second[1]));
 
         var instructions = input.Skip(crates.Count() + 1).Select(i => ParseMove(i, stacks));
-        instructions.ForEach(instr => Enumerable.Range(0, instr.CratesToMove).ForEach(_ => instr.To.Push(instr.From.Pop())));
 
-        return string.Join("", stacks.Select(s => s.Pop()));
-    }
-    public override object Part2(List<string> input)
-    {
-        return 0;
+        return (stacks, instructions);
     }
 
     private MoveInstruction ParseMove(string move, IEnumerable<Stack<char>> stacks)
