a119a6fbac277078dd19a4b8123a29175e8d486c
jarosenius <jakob@arosenius.com>
2023-01-04 14:29:33 +0100

    Part 1

--- 2022/Day05.cs
+++ 2022/Day05.cs
@@ -11,13 +11,33 @@ public class Day05 : Day
 
     }
 
-    public override long Part1(List<string> input)
+    public override object Part1(List<string> input)
+    {
+        var crates = input.TakeWhile(i => i != string.Empty);
+        var stacks = crates.Last().Chunk(4).Select(c => new Stack<char>()).ToArray();
+        var cratesWithLetter = crates.Reverse().Skip(1).SelectMany(c => stacks.Zip(c.Chunk(4)));
+        cratesWithLetter.Where(c => c.Second[1] != ' ').ForEach(c => c.First.Push(c.Second[1]));
+
+        var instructions = input.Skip(crates.Count() + 1).Select(i => ParseMove(i, stacks));
+        instructions.ForEach(instr => Enumerable.Range(0, instr.CratesToMove).ForEach(_ => instr.To.Push(instr.From.Pop())));
+
+        return string.Join("", stacks.Select(s => s.Pop()));
+    }
+    public override object Part2(List<string> input)
     {
         return 0;
     }
-    public override long Part2(List<string> input)
+
+    private MoveInstruction ParseMove(string move, IEnumerable<Stack<char>> stacks)
     {
-        return 0;
+        var numbers = move.Replace("move ", "").Replace("from ", "").Replace("to ", "").Trim().Split(" ").Select(int.Parse).ToArray();
+        var numberToMove = numbers[0];
+        var from = numbers[1];
+        var to = numbers[2];
+
+        return new MoveInstruction(numberToMove, stacks.ElementAt(from - 1), stacks.ElementAt(to - 1));
     }
+
+    private record MoveInstruction(int CratesToMove, Stack<char> From, Stack<char> To);
 }
 
