27ad10b791ad849424aa120795842e3acd12a2cc
jarosenius <jakob@arosenius.com>
2023-12-01 21:18:26 +0100

    Day 1 - Part 2

--- 2023/Day01.cs
+++ 2023/Day01.cs
@@ -14,15 +14,33 @@ public class Day01 : Day
 
     public override object Part1(List<string> input)
     {
-        return input
-            .Select(row => 
-                int.Parse(
-                    $"{Regex.Match(row, @"\d")}{Regex.Match(row, @"\d", RegexOptions.RightToLeft)}")
-            ).Sum();
+        return input.Select(row => GetNumber(row, @"\d")).Sum();
     }
     public override object Part2(List<string> input)
     {
-        return 0;
+        return input.Select(row => GetNumber(row, @"\d|one|two|three|four|five|six|seven|eight|nine")).Sum();
     }
+
+    private int GetNumber(string row, string pattern)
+    {
+        var first = Regex.Match(row, pattern).Value;
+        var last = Regex.Match(row, pattern, RegexOptions.RightToLeft).Value;
+        return GetNumber(first)*10 + GetNumber(last);
+    }
+
+    private int GetNumber (string num) => num switch
+        {
+            "" => 0,
+            "one" => 1,
+            "two" => 2,
+            "three" => 3,
+            "four" => 4,
+            "five" => 5,
+            "six" => 6,
+            "seven" => 7,
+            "eight" => 8,
+            "nine" => 9,
+            _ => int.Parse(num)
+        };
 }
 
