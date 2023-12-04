a63210523dbc59db2c3f8de68b8283848f4b0958
jarosenius <jakob@arosenius.com>
2023-12-04 09:31:11 +0100

    Day 4 improvements

--- 2023/Day04.cs
+++ 2023/Day04.cs
@@ -15,42 +15,32 @@ public class Day04 : Day
 
     public override object Part1(List<string> input)
     {
-        return CalculateCards(input).Sum(g => g.Value);
+        return CalculateCards(input).Sum(g => Math.Pow(2, g.TotalWinningNumbers-1));
     }
     public override object Part2(List<string> input)
     {
         var cards = CalculateCards(input).OrderBy(c => c.Id).ToList();
-        var result = new ConcurrentDictionary<int, long>();
-        cards.ForEach(c => CalculateWinnings(c, cards, result));
+        var result = cards.ToDictionary(kvp => kvp.Id, _ => 1);
+        foreach (var card in cards)
+        {
+            var (Id, Counts) = (cards[card.Id-1], result[card.Id]);
+            for (int j = 0; j < card.TotalWinningNumbers; j++)
+            {
+                result[card.Id+j+1] += Counts;
+            }
+        }
         return result.Sum(kvp => kvp.Value);
     }
 
-    private static void CalculateWinnings((int Id, long Value, int TotalWinningNumbers) card, IEnumerable<(int Id, long Value, int TotalWinningNumbers)> cards, ConcurrentDictionary<int, long> result)
-    {
-        result.AddOrUpdate(card.Id, 1, (key, val) => val+1);
-        if(card.TotalWinningNumbers == 0)
-            return;
-        var copies = cards.Skip(card.Id).Take(card.TotalWinningNumbers).ToList();
-        copies.ForEach(c => CalculateWinnings(c, cards, result));
-    }
-
-    private static IEnumerable<(int Id, long Value, int TotalWinningNumbers)> CalculateCards(List<string> input)
+    private static IEnumerable<(int Id, int TotalWinningNumbers)> CalculateCards(List<string> input)
     {
         return input.Select(row =>
         {
-            var parts = row.Split(":");
+            var parts = row.Split(':', '|');
             var id = int.Parse(parts[0].Replace(" ", "")[4..]);
-            var numbers = parts[1].Split(" | ").ToArray();
-            var scratched = numbers[0].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToHashSet();
-            var winning = numbers[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(w => w.Trim()).ToHashSet();
-            var value = scratched.Aggregate(0L, (total, num) => {
-                if(!winning.Contains(num))
-                    return total;
-                if(total == 0)
-                    return 1;
-                return total*2;
-            });
-            return (Id: id, Value: value, scratched.Count(s => winning.Contains(s)));
+            var scratched = parts[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToHashSet();
+            var winning = parts[2].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(w => w.Trim()).ToHashSet();
+            return (Id: id, scratched.Count(s => winning.Contains(s)));
         });
     }
 }
