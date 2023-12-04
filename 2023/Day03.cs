752be1389e0dba3c66c4545b70436db2e8ae5136
jarosenius <jakob@arosenius.com>
2023-12-04 07:58:42 +0100

    Common day 3

--- 2023/Day03.cs
+++ 2023/Day03.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,78 +13,52 @@ public class Day03 : Day
     public override object Part1(List<string> input)
     {
         var map = input.Select(row => row.ToCharArray()).ToArray();
-        var width = map[0].Length;
-        var height = map.Length;
 
         var sum = 0;
         var number = 0;
         var isPartNumber = false;
-        for (var y = 0; y < height; y++)
+        CalculatePartNumbers(map, 
+        ifIsDigit: (c, x, y, _, _) => 
         {
-            for (int x = 0; x < width; x++)
-            {
-                var c = map[y][x];
-                if (char.IsDigit(c))
-                {
-                    number = number * 10 + (c - '0');
-                    var neighbors = map.GetNeighbors(x, y, true);
-                    if (neighbors.Any(n => n != '\0' && !char.IsDigit(n) && n != '.'))
-                        isPartNumber = true;
-                }
-                else
-                    NewNumber();
-            }
-            NewNumber();
-        }
-
-        return sum;
-
-        void NewNumber()
+            number = number * 10 + (c - '0');
+            var neighbors = map.GetNeighbors(x, y, true);
+            if (neighbors.Any(n => n != '\0' && !char.IsDigit(n) && n != '.'))
+                isPartNumber = true;
+        },
+        reset: () => 
         {
             if (isPartNumber)
                 sum += number;
             number = 0;
             isPartNumber = false;
-        }
+        });
+
+        return sum;
     }
 
     public override object Part2(List<string> input)
     {
         var map = input.Select(row => row.ToCharArray()).ToArray();
-        var width = map[0].Length;
-        var height = map.Length;
 
         var gears = new Dictionary<(int X, int Y), List<int>>();
         var neighbors = new HashSet<(int X, int Y)>();
         var number = 0;
-        for (var y = 0; y < height; y++)
+
+        CalculatePartNumbers(map, 
+        ifIsDigit: (c, x, y, width, height) => 
         {
-            for (int x = 0; x < width; x++)
+            number = number * 10 + (c - '0');
+            Offsets.ForEach(o =>
             {
-                var c = map[y][x];
-
-                if (char.IsDigit(c))
-                {
-                    number = number * 10 + (c - '0');
-                    Offsets.ForEach(o =>
-                    {
-                        var nX = x + o.X;
-                        var nY = y + o.Y;
-                        if (nX < 0 || nY < 0 || nX >= width || nY >= height)
-                            return;
-                        if (map[nY][nX] == '*')
-                            neighbors.Add((nX, nY));
-                    });
-                }
-                else
-                    NewNumber();
-            }
-            NewNumber();
-        }
-
-        return gears.Where(g => g.Value.Count == 2).Select(g => g.Value[0] * g.Value[1]).Sum();
-
-        void NewNumber()
+                var nX = x + o.X;
+                var nY = y + o.Y;
+                if (nX < 0 || nY < 0 || nX >= width || nY >= height)
+                    return;
+                if (map[nY][nX] == '*')
+                    neighbors.Add((nX, nY));
+            });
+        }, 
+        reset: () => 
         {
             if (neighbors.Count > 0)
             {
@@ -98,6 +73,29 @@ public class Day03 : Day
 
             number = 0;
             neighbors.Clear();
+        });
+
+        return gears.Where(g => g.Value.Count == 2).Select(g => g.Value[0] * g.Value[1]).Sum();
+    }
+
+    private static void CalculatePartNumbers(char[][] map, Action<char, int, int, int, int> ifIsDigit, Action reset)
+    {
+        var width = map[0].Length;
+        var height = map.Length;
+        for (var y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                var c = map[y][x];
+
+                if (char.IsDigit(c))
+                {
+                    ifIsDigit(c, x, y, width, height);
+                }
+                else
+                    reset();
+            }
+            reset();
         }
     }
 
