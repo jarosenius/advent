19312fb7450a9589caf95424930a74edf8ce9505
jarosenius <jakob@arosenius.com>
2023-12-06 21:29:34 +0100

    Day 6 - Part 1

--- 2023/Day06.cs
+++ 2023/Day06.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Advent.y2023;
 
@@ -13,11 +15,24 @@ public class Day06 : Day
 
     public override object Part1(List<string> input)
     {
-        return 0;
+        var infos = GetInfo(input);
+        return infos.Aggregate(1L, (res, i) => res *= Enumerable.Range(0, i.Time+1).Count(l => IsRecord(i, l)));
     }
+
     public override object Part2(List<string> input)
     {
         return 0;
     }
+
+    private static bool IsRecord(Info info, long hold) => (info.Time - hold) * hold > info.Distance;
+
+    private static IEnumerable<Info> GetInfo(List<string> input)
+    {
+        var time = Regex.Matches(input[0], @"\d+").Select(m => int.Parse(m.Value));
+        var distance = Regex.Matches(input[1], @"\d+").Select(m => long.Parse(m.Value));
+        return time.Zip(distance).Select(z => new Info(z.First, z.Second));
+    }
+
+    private record Info(int Time, long Distance);
 }
 
