8af439bcc8e16cb9a002fa8dbf0e3dd016a70650
Jakob Arosenius <7369512+jarosenius@users.noreply.github.com>
2023-12-08 09:03:59 +0100

    Day 8 - Part 1

--- 2023/Day08.cs
+++ 2023/Day08.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -13,11 +14,37 @@ public class Day08 : Day
 
     public override object Part1(List<string> input)
     {
-        return 0;
+        return GetSteps(input);
     }
     public override object Part2(List<string> input)
     {
         return 0;
     }
-}
 
+    private static int GetSteps(List<string> input)
+    {
+        var order = input[0];
+        var map = input.Skip(2).Select(i =>{
+            var parts = i.Split(" = ");
+            var leftRight = parts[1].Split(", ");
+            var kvp = new KeyValuePair<string, (string Left, string Right)>(parts[0], (leftRight[0][1..], leftRight[1][..^1]));
+            return kvp;
+        }).ToDictionary();
+
+
+        var count = 0;
+        var current = "AAA";
+        do
+        {
+            var step = order[count++%order.Length];
+            current = step switch
+            {
+                'R' => map[current].Right,
+                'L' => map[current].Left,
+                _ => throw new ArgumentException("Invalid input"),
+            };
+        } while (current != "ZZZ");
+        
+        return count;
+    }
+}
