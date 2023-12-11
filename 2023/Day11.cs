6597ea9a769afc375f0a361b8193b78ca2caa945
jarosenius <jakob@arosenius.com>
2023-12-11 11:53:24 +0100

    Day 11 - Part 1 & 2

--- 2023/Day11.cs
+++ 2023/Day11.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 
 namespace Advent.y2023;
@@ -11,13 +13,40 @@ public class Day11 : Day
 
     }
 
-    public override object Part1(List<string> input)
+    public override object Part1(List<string> input) => GetResult(input);
+
+    public override object Part2(List<string> input) => GetResult(input, 1000000);
+
+    private static long GetResult(List<string> input, int expansion = 2)
     {
-        return 0;
+        var galaxies = GetCoordinatesForGalaxies(input);
+        var emptyRows = GetEmptyRows(input);
+        var emptyColumns = GetEmptyColumns(input);
+        var pairs = galaxies.SelectMany((first, i) => galaxies.Skip(i+1).Select(second => (First: first, Second: second)));
+        return pairs.Select(p => DistanceBetweenPoints(p.First, p.Second, expansion, emptyRows, emptyColumns)).Sum();
     }
-    public override object Part2(List<string> input)
+
+    private static List<Point> GetCoordinatesForGalaxies(List<string> input) => 
+        Enumerable.Range(0, input.Count)
+        .SelectMany(y => 
+            Enumerable.Range(0, input[0].Length)
+            .Where(x => input[y][x] == '#')
+            .Select(x => new Point(x, y)))
+        .ToList();
+
+    private static long DistanceBetweenPoints(Point first, Point second, int expansion, List<int> emptyRows, List<int> emptyColumns)
     {
-        return 0;
+        var diffX = Math.Abs(first.X - second.X);
+        var diffY = Math.Abs(first.Y - second.Y);
+        return diffX + diffY + Expansion(diffX, first.X, second.X, false) + Expansion(diffY, first.Y, second.Y, true);
+        long Expansion(int diff, int a, int b, bool row)
+        {
+            var emptySpace = Enumerable.Range(Math.Min(a, b), diff).Count(i => row ? emptyRows.Contains(i) : emptyColumns.Contains(i));
+            return (expansion-1)*emptySpace;
+        }
     }
+        
+    private static List<int> GetEmptyRows(List<string> input) => Enumerable.Range(0, input.Count).Where(y => input[y].All(c => c == '.')).ToList();
+    private static List<int> GetEmptyColumns(List<string> input) => Enumerable.Range(0, input[0].Length).Where(x => input.All(row => row[x] == '.')).ToList();
 }
 
