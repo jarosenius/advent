dde5552622241fcfb65d6550a7e25173470ea3f9
Jakob Arosenius <7369512+jarosenius@users.noreply.github.com>
2023-12-21 08:09:46 +0100

    Day21 - part 1

--- 2023/Day21.cs
+++ 2023/Day21.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using Advent.Common;
 
 namespace Advent.y2023;
 
@@ -13,11 +14,33 @@ public class Day21 : Day
 
     public override object Part1(List<string> input)
     {
-        return null;
+        var map = Coordinate.CreateMap(input);
+        HashSet<Coordinate> plots = [map.Single(m => m.Value == 'S').Key];
+        for (int i = 0; i < 64; i++)
+        {
+            plots = plots.SelectMany(p => GetPossibleStepsFrom(p, map)).ToHashSet();
+        }
+        return plots.Count;
     }
     public override object Part2(List<string> input)
     {
         return null;
     }
-}
 
+
+    private static IEnumerable<Coordinate> GetPossibleStepsFrom(Coordinate current, Dictionary<Coordinate, char> map)
+    {
+        var up = current+Direction.Up;
+        var right = current+Direction.Right;
+        var down = current+Direction.Down;
+        var left = current+Direction.Left;
+        if(map.TryGetValue(up, out var uStep) && uStep != '#')
+            yield return up;
+        if(map.TryGetValue(right, out var rStep) && rStep != '#')
+            yield return right;
+        if(map.TryGetValue(down, out var dStep) && dStep != '#')
+            yield return down;
+        if(map.TryGetValue(left, out var lStep) && lStep != '#')
+            yield return left;
+    }
+}
