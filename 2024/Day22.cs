b9111195bd63547be61d516f67f38fb2cd174315
jarosenius <jakob@arosenius.com>
2024-12-02 12:24:29 +0100

    Adding the remaining days for 2024

---
+++ 2024/Day22.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Linq;
+using Advent.Common;
+
+namespace Advent.y2024;
+
+[AoC(2024)]
+public class Day22() : Day(22, 2024)
+{
+    public override object Part1(List<string> input)
+    {
+        return null;
+    }
+    public override object Part2(List<string> input)
+    {
+        return null;
+    }
+}
+
