78c15b110d511af272c627042414a43be6b68da6
Jakob Arosenius <7369512+jarosenius@users.noreply.github.com>
2024-12-03 08:17:44 +0100

    Day 3 part 1 & 2

--- 2024/Day03.cs
+++ 2024/Day03.cs
@@ -9,15 +9,20 @@ public class Day03() : Day(3, 2024)
 {
     public override object Part1(List<string> input)
     {
-        var pattern = "(mul\((\d{1,3}),(\d{1,3})\))";
-        var reg = new Regex(pattern, RegexOptions.Compiled);
-        var mem = string.Join("",input);
-        var match = reg.Matches(mem);
-        return null;
+        var reg = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)", RegexOptions.Compiled);
+        return reg.Matches(string.Join("", input)).Sum(m => int.Parse(m.Groups[1].Value) * int.Parse(m.Groups[2].Value));
     }
     public override object Part2(List<string> input)
     {
-        return null;
+        var reg = new Regex(@"do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)", RegexOptions.Compiled);
+        var matches = reg.Matches(string.Join("", input));
+        return matches.Aggregate((0,false), (data, m) =>
+        {
+            var skip = (m.Groups[0].Value == "don't()" || data.Item2) && m.Groups[0].Value != "do()";
+            return skip || m.Groups[0].Value == "do()" 
+                ? data with { Item2 = skip } 
+                : data with { Item1 = data.Item1 + int.Parse(m.Groups[1].Value) * int.Parse(m.Groups[2].Value), Item2 = skip };
+        }).Item1;
     }
 }
 
