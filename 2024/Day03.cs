d868743e824d6be4e61d1e187aebaa0b7ab0608c
Jakob Arosenius <7369512+jarosenius@users.noreply.github.com>
2024-12-03 07:09:25 +0100

    wip day03 from phone

--- 2024/Day03.cs
+++ 2024/Day03.cs
@@ -9,6 +9,10 @@ public class Day03() : Day(3, 2024)
 {
     public override object Part1(List<string> input)
     {
+        var pattern = "(mul\((\d{1,3}),(\d{1,3})\))";
+        var reg = new Regex(pattern, RegexOptions.Compiled);
+        var mem = string.Join("",input);
+        var match = reg.Matches(mem);
         return null;
     }
     public override object Part2(List<string> input)
