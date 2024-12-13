b296459a69cfe56c921cd3c90a20ae9125de90d2
Jakob Arosenius <7369512+jarosenius@users.noreply.github.com>
2024-12-13 14:20:49 +0100

    Update comments

--- Common/LinearDiophantineEquations.cs
+++ Common/LinearDiophantineEquations.cs
@@ -5,15 +5,13 @@ namespace Advent.Common
         public static (long A, long B) Solve((long X, long Y) a, (long X, long Y) b, (long A, long B) Target)
         {
             // aX + bY = c
-            // a.X*minA + b.X*Y = Prize.X
-            // a.Y*minA + b.Y*Y = Prize.Y
-            // minA = (Prize.X-b.X*Y) / a.X
+            // a.X*X + b.X*Y = Prize.X
+            // a.Y*X + b.Y*Y = Prize.Y
+            // X = (Prize.X-b.X*Y) / a.X
             // a.Y * (Prize.X - b.X*Y)/a.X + b.Y*Y = Prize.Y
             // (a.Y*Prize.X - a.Y*b.X*Y) / a.X + b.Y*Y = Prize.Y
             // (a.Y*Prize.X - a.Y*b.X*Y) + b.Y*a.X*Y = Prize.Y * a.X
             // Y = (Prize.Y * a.X - Prize.X*a.Y) / (b.Y*a.X - b.X*a.Y)
-            // X = Prize.X - b.X * Y
-            // X = (Prize.X-b.X * Y) / a.X
             long Y = (Target.B * a.X - Target.A * a.Y) / (b.Y * a.X - b.X * a.Y);
             long X = (Target.A - b.X * Y) / a.X;
 
