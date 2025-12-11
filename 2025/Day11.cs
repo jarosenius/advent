1ae1cb8a38ada9b18811385734523f9f78d07099
jarosenius <jakob@arosenius.com>
2025-12-11 06:29:02 +0100

    Day 11 part 2

--- 2025/Day11.cs
+++ 2025/Day11.cs
@@ -1,19 +1,14 @@
 using System.Collections.Generic;
 using System.Linq;
-using Advent.Common;
-using MoreLinq;
 
 namespace Advent.y2025;
 
 [AoC(2025)]
 public class Day11() : Day(11, 2025)
 {
-    public override object Part1(List<string> input) => CountPaths(Parse(input), "you", "out", []);
+    public override object Part1(List<string> input) => CountPaths(Parse(input), "you", "out", [], [], []);
 
-    public override object Part2(List<string> input)
-    {
-        return null;
-    }
+    public override object Part2(List<string> input) => CountPaths(Parse(input), "svr", "out", [], ["dac", "fft"], []);
 
     private static Dictionary<string, List<string>> Parse(List<string> input) =>
         input.Select(line => line.Split(": "))
@@ -21,19 +16,34 @@ public class Day11() : Day(11, 2025)
                 parts => parts[0],
                 parts => parts[1].Split(' ').ToList());
 
-    private static int CountPaths(Dictionary<string, List<string>> graph, string current, string target, HashSet<string> visited)
+    private static long CountPaths(
+        Dictionary<string, List<string>> graph,
+        string current,
+        string target,
+        HashSet<string> visited,
+        HashSet<string> requiredNodes,
+        Dictionary<string, long> mem)
     {
         if (current == target)
-            return 1;
+            return requiredNodes.Count == 0 ? 1 : 0;
 
-        if (!(graph.TryGetValue(current, out var value) && !visited.Contains(current)))
+        if (!graph.TryGetValue(current, out var outputs) || visited.Contains(current))
             return 0;
 
+        var key = $"{current}:{string.Join(",", requiredNodes.OrderBy(x => x))}";
+        if (mem.TryGetValue(key, out var cachedResult))
+            return cachedResult;
+
         visited.Add(current);
+        var required = requiredNodes.Remove(current);
 
-        var count = value.Sum(next => CountPaths(graph, next, target, visited));
+        var count = outputs.Sum(next => CountPaths(graph, next, target, visited, requiredNodes, mem));
+
         visited.Remove(current);
+        if (required)
+            requiredNodes.Add(current);
 
+        mem[key] = count;
         return count;
     }
 }
